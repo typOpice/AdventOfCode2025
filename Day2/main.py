6f6800a1f3e72a7133bc84baca2030307cd70e5b
Michael Rodriguez <mdr721.edu@gmail.com>
2025-12-14 22:08:46 -0500

    Build list of ID pairs from raw IDs and print

--- Day2/main.py
+++ Day2/main.py
@@ -7,6 +7,15 @@ def main():
     l = list(map(lambda x: x.strip(","), lines))
     RawlistOfIDs = list(map(lambda x: x.split("-"), l)).pop(0)
 
+    listOfIDs = []
+    for i in range(0, len(RawlistOfIDs) - 2, 2):
+        t = []
+        t.append(RawlistOfIDs[i])
+        t.append(RawlistOfIDs[i + 1])
+        listOfIDs.append(t)
+
+    print(listOfIDs)
+
 
 def checkForDoubles():
     pass
